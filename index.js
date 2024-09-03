3cea2474d29828997e91ab5fdd4772502dfd25d6
ahmetcanisik <can.isik.business@gmail.com>
2024-09-03 22:05:29 +0300

    v0.0.2

---
+++ index.js
@@ -0,0 +1,90 @@
+const { parcol } = require("parcol");
+class Logil {
+    #BASE_CONFIG = {
+        icon: "▬",
+        prefix: "Logil",
+        level: 0
+    }
+
+    #prefix(message, icon_color) {
+        return parcol.pit(`~${icon_color} ${this.#BASE_CONFIG.icon}~ ~rs,d ${this.#BASE_CONFIG.prefix}: ~ ${message}`);
+    }
+
+    /**
+     * 0: li
+     * 1: nice
+     * 2: info
+     * 3: warn
+     * 4: error
+     */
+    #log(message, level) {
+        switch (level) {
+            case 1:
+                console.log(this.#prefix(message, "gb"));
+                break;
+            case 2:
+                console.log(this.#prefix(message, "blb"));
+                break;
+            case 3:
+                console.log(this.#prefix(message, "yb"));
+                break;
+            case 4:
+                console.log(this.#prefix(message, "rb"));
+                break;
+            default:
+                console.log(this.#prefix(message, "cb"));
+                break;
+        }
+    }
+
+    congigure(options = this.#BASE_CONFIG) {
+        this.#BASE_CONFIG = {
+            ...this.#BASE_CONFIG,
+            ...options
+        }
+    }
+
+    /**
+     * @mean default, normal
+     * @level 0
+     */
+    li(message) {
+        this.#log(message, 0);
+    }
+
+    /**
+     * @mean success
+     * @level 1
+     */
+    nice(message) {
+        this.#log(message, 1);
+    }
+
+    /**
+     * @mean information
+     * @level 2
+     */
+    info(message) {
+        this.#log(message, 2);
+    }
+
+    /**
+     * @mean warning
+     * @level 3
+     */
+    warn(message) {
+        this.#log(message, 3);
+    }
+
+    /**
+     * @mean error
+     * @level 4
+     */
+    error(message) {
+        this.#log(message, 4);
+    }
+}
+
+module.exports = {
+    log: new Logil()
+};
