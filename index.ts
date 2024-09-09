195c3bd46c11d5460f93291876f799fefd80394f
ahmetcanisik <can.isik.business@gmail.com>
2024-09-09 17:24:48 +0300

    updated to 0.0.7

--- index.ts
+++ index.ts
@@ -1,22 +1,37 @@
 import parcol from 'parcol';
+
+interface LogilConfig {
+    icon: string,
+    prefix: string,
+    level: number
+}
+
+interface PrefixInterface {
+    noPrefix: boolean
+}
+
 export class Logil {
-    #BASE_CONFIG = {
+    #BASE_CONFIG: LogilConfig = {
         icon: "▬",
         prefix: "Logil",
         level: 0
     };
-    #BASE_PREFIX = {
+
+    #BASE_PREFIX: PrefixInterface = {
         noPrefix: false
-    };
-    constructor(config = this.#BASE_CONFIG) {
+    }
+
+    constructor(config: LogilConfig = this.#BASE_CONFIG) {
         this.#BASE_CONFIG = config;
     }
-    #prefix(message, icon_color, options = this.#BASE_PREFIX) {
+
+    #prefix(message: string, icon_color: string, options: PrefixInterface = this.#BASE_PREFIX) {
         if (options && options.noPrefix) {
             return parcol.pit(message);
         }
         return parcol.pit(`~${icon_color} ${this.#BASE_CONFIG.icon}~ ~rs,d ${this.#BASE_CONFIG.prefix}: ~ ${message}`);
     }
+
     /**
      * 0: il
      * 1: nice
@@ -24,7 +39,7 @@ export class Logil {
      * 3: warn
      * 4: error
      */
-    #log(message, level, options = this.#BASE_PREFIX) {
+    #log(message: string, level: number, options: PrefixInterface = this.#BASE_PREFIX) {
         switch (level) {
             case 1:
                 console.log(this.#prefix(message, "gb", options));
@@ -43,43 +58,50 @@ export class Logil {
                 break;
         }
     }
-    configure(options) {
+
+    configure(options: LogilConfig) {
         this.#BASE_CONFIG = options;
     }
+
     /**
      * @mean logil(log.il), default, normal
      * @level 0
      */
-    il(message, options = this.#BASE_PREFIX) {
+    il(message: string, options: PrefixInterface = this.#BASE_PREFIX) {
         this.#log(message, 0, options);
     }
+
     /**
      * @mean success
      * @level 1
      */
-    nice(message, options = this.#BASE_PREFIX) {
+    nice(message: string, options: PrefixInterface = this.#BASE_PREFIX) {
         this.#log(message, 1, options);
     }
+
     /**
      * @mean information
      * @level 2
      */
-    info(message, options = this.#BASE_PREFIX) {
+    info(message: string, options: PrefixInterface = this.#BASE_PREFIX) {
         this.#log(message, 2, options);
     }
+
     /**
      * @mean warning
      * @level 3
      */
-    warn(message, options = this.#BASE_PREFIX) {
+    warn(message: string, options: PrefixInterface = this.#BASE_PREFIX) {
         this.#log(message, 3, options);
     }
+
     /**
      * @mean error
      * @level 4
      */
-    error(message, options = this.#BASE_PREFIX) {
+    error(message: string, options: PrefixInterface = this.#BASE_PREFIX) {
         this.#log(message, 4, options);
     }
 }
+
 export default new Logil();
--- test.ts
+++ test.ts
@@ -1,6 +1,9 @@
 import log from "./index.js";
+
 log.info("started... ~c go!~");
+
 log.error("where is she?");
+
 log.info("~b test~", {
     noPrefix: true
-});
+})
